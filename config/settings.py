7b33edb1a83f8889d99edae202050961be0f386d
Django <django@hermes>
2026-08-19 15:15:59 -0400

    Fix URL namespacing and nginx proxy config

--- config/settings.py
+++ config/settings.py
@@ -88,8 +88,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-LOGIN_URL = 'login'
+LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = 'dashboard'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'investigator:home'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
